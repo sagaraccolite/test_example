9c59e70209efb68de5a5c230ba860063b7b50e73
Sagar Mourya <sagar.mourya@accoliteindia.com>
2020-10-20 18:43:20 +0530

    Update after make branch

--- Addition.java
+++ Addition.java
@@ -5,6 +5,6 @@ public class Addition {
 	public static void main(String[] args) {
 		int a=10;
 		int b=20;
-		System.out.println("Addition of 10+20 is =  "+(a+b));
+		System.out.println("Addition of  two  number 10+20 is =  "+(a+b));
 	}
 }

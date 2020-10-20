4b5f072a25ceb81013aa752cbe92707ed357fcb6
Sagar Mourya <sagar.mourya@accoliteindia.com>
2020-10-20 16:17:45 +0530

    Update in Add

--- Addition.java
+++ Addition.java
@@ -5,6 +5,6 @@ public class Addition {
 	public static void main(String[] args) {
 		int a=10;
 		int b=20;
-		System.out.println("Add no "+(a+b));
+		System.out.println("Add two no "+(a+b));
 	}
 }

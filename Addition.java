dda3028518e5c6e74edf21b6642c4c12e0e74a43
Sagar Mourya <sagar.mourya@accoliteindia.com>
2020-10-21 11:59:57 +0530

    add 70+70

--- Addition.java
+++ Addition.java
@@ -3,8 +3,8 @@ package com.test;
 public class Addition {
 
 	public static void main(String[] args) {
-		int a=30;
-		int b=40;
-		System.out.println("Addition of 30+40 is =  "+(a+b));
+		int a=70;
+		int b=70;
+		System.out.println("Addition of 70+70 is =  "+(a+b));
 	}
 }

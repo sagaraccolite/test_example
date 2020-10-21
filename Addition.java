382fdceb3275310eac2cf605b6b6f69a9a14210a
Sagar Mourya <sagar.mourya@accoliteindia.com>
2020-10-21 15:25:31 +0530

    correct sop

--- Addition.java
+++ Addition.java
@@ -5,6 +5,6 @@ public class Addition {
 	public static void main(String[] args) {
 		int a=21;
 		int b=12;
-		System.out.println("new Addition of 2+1 is =  "+(a+b));
+		System.out.println("new Addition of 21+12 is =  "+(a+b));
 	}
 }

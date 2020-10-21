6dcbe0544e584f7966a3332c3dc9ba40e577fa50
Sagar Mourya <sagar.mourya@accoliteindia.com>
2020-10-21 15:18:11 +0530

    check it going to clone or not

--- Addition.java
+++ Addition.java
@@ -3,8 +3,8 @@ package com.test;
 public class Addition {
 
 	public static void main(String[] args) {
-		int a=2;
-		int b=1;
+		int a=21;
+		int b=12;
 		System.out.println("new Addition of 2+1 is =  "+(a+b));
 	}
 }

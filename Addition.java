d1bc54a3d3889a0aabe9ff9ee1723aaa3223a356
Sagar Mourya <sagar.mourya@accoliteindia.com>
2020-10-21 16:11:37 +0530

    form branch1 modification

--- Addition.java
+++ Addition.java
@@ -5,6 +5,6 @@ public class Addition {
 	public static void main(String[] args) {
 		int a=31;
 		int b=13;
-		System.out.println("branch1 Addition of 31+13 is =  "+(a+b));
+		System.out.println(" from branch1 Addition of 31+13 is =  "+(a+b));
 	}
 }
--- Division.java
+++ Division.java
@@ -4,6 +4,6 @@ public class Division {
 	public static void main(String[] args) {
 		int a=8;
 		int b=2;
-		System.out.println("branch1 Division of 8/2 is = "+(a/b));
+		System.out.println("from branch1 Division of 8/2 is = "+(a/b));
 	}
 }

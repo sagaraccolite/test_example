258ca12231b904c8506abf670def727d9dfc0803
Sagar Mourya <sagar.mourya@accoliteindia.com>
2020-10-21 19:15:51 +0530

    remove from branch2

--- Addition.java
+++ Addition.java
@@ -5,6 +5,6 @@ public class Addition {
 	public static void main(String[] args) {
 		int a=31;
 		int b=13;
-		System.out.println(" fork test branch2 Addition of 31+13 is =  "+(a+b));
+		System.out.println("Addition of 31+13 is =  "+(a+b));
 	}
 }

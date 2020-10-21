989f9c080a8843acc4ff2c24f755f6b1d8f65749
Sagar Mourya <sagar.mourya@accoliteindia.com>
2020-10-21 19:17:17 +0530

    use extra sop

--- Addition.java
+++ Addition.java
@@ -5,6 +5,7 @@ public class Addition {
 	public static void main(String[] args) {
 		int a=31;
 		int b=13;
-		System.out.println(" fork test branch2 Addition of 31+13 is =  "+(a+b));
+		System.out.println("Addition two number");
+		System.out.println("Addition of 31+13 is =  "+(a+b));
 	}
 }

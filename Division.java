23f7e87765b774c68f65c2bf0b16825764ac7dcb
sagaraccolite <73175312+sagaraccolite@users.noreply.github.com>
2020-10-22 11:52:02 +0530

    update to 81 and 12

--- Division.java
+++ Division.java
@@ -2,8 +2,8 @@ package com.test;
 
 public class Division {
 	public static void main(String[] args) {
-		int a=8;
-		int b=2;
-		System.out.println("Division of 8/2 is = "+(a/b));
+		int a=81;
+		int b=12;
+		System.out.println("Division of 81/12 is = "+(a/b));
 	}
 }

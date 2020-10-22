8bd38fb7a459371a983ec94d92ec629664a9fb1f
sagaraccolite <73175312+sagaraccolite@users.noreply.github.com>
2020-10-22 18:11:25 +0530

    Update AC to XY

--- Addition.java
+++ Addition.java
@@ -5,6 +5,6 @@ public class Addition {
 	public static void main(String[] args) {
 		int x=10;
 		int y=20;
-		System.out.println("Addition of 10,20 is =  "+(a+c));
+		System.out.println("Addition of 10+20 is =  "+(x+y));
 	}
 }

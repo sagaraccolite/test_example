5f758307dc7686f4375e1e72465697e73ad2fe35
Sagar Mourya <sagar.mourya@accoliteindia.com>
2020-10-22 18:21:32 +0530

    pq

--- Addition.java
+++ Addition.java
@@ -3,8 +3,8 @@ package com.test;
 public class Addition {
 
 	public static void main(String[] args) {
-		int m=30;
-		int n=40;
-		System.out.println("Addition of 30+40 is =  "+(m+n));
+		int p=101;
+		int q=202;
+		System.out.println("Addition of p,q is =  "+(p+q));
 	}
 }
